e324e8399b36b7c3e2d773b376eb4f36f7a509ca
Juan Pablo López Guzmán <juanpablo@findasense.com>
2018-06-08 15:44:02 -0500

    Schema API Pastebin API like done

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 # Create a router and regisster out viewsets with it
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
 ]
