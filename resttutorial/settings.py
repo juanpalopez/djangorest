8b477bd7977226f9119006c5b8656ab83c3989f4
Juan Pablo López Guzmán <juanpablo@findasense.com>
2018-06-08 13:59:23 -0500

    Hyperlinking and Browsability

--- resttutorial/settings.py
+++ resttutorial/settings.py
@@ -101,6 +101,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# REST FRAMEWORK settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,15 +1,15 @@
-# from django.conf.urls import url
+from django.conf.urls import url
 from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     re_path(r'^$', views.api_root),
-    re_path(r'^snippets/$', views.SnippetList.as_view()),
-    re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    re_path(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
-    re_path(r'^users/$', views.UserList.as_view()),
-    re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    re_path(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    re_path(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    re_path(r'^users/$', views.UserList.as_view(), name='user-list'),
+    re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
